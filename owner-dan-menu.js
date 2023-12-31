7e71ab26ac3515040c2d73fc3f75aed839c74964
min1933 <namaandasip@gmail.com>
2023-12-31 12:23:51 +0700

    bot

---
+++ owner-dan-menu.js
@@ -0,0 +1,85 @@
+const fs = require('fs')
+
+global.namabot = "𝑷𝑼𝑻𝒁 𝑮𝑵𝒁"
+global.namaowner = "𝑮𝑵𝒁"
+global.footer_text = "© 𝑮𝑵𝒁" + namabot
+global.pp_bot = fs.readFileSync("./image/foto.jpg")
+global.qris = fs.readFileSync("./image/qris.jpg")
+global.owner = ['6283820352529','6283820352529']
+global.sessionName = 'session'
+global.prefa = ['-_-']
+global.caption_pay = `Qris All Pay Bisa Pm Aja Ke Owner
+Dana: 083820352529
+Gopay: 083110933360
+Ovo: 083820352529
+
+Mau ganti payment? ketik .setpay
+`
+//menu bot rapihin sendiri ya, belajar lah jadi anak mandiri.
+module.exports.helpMenu = (pushname) =>{
+  return `Halo ${pushname}
+
+*「 FITUR BOT 」*
+
+\`\`\`» /owner
+» /addsewa
+» /delsewa
+» /ceksewa
+» /listsewa
+» /pay
+» /setpay
+» /list
+» /addlist
+» /updatelist
+» /renamelist
+» /dellist
+» /jeda
+» /tambah
+» /kurang
+» /kali
+» /bagi
+» /setproses
+» /changeproses
+» /delsetproses
+» /setdone
+» /changedone
+» /delsetdone
+» /proses
+» /done
+» /welcome
+» /goodbye
+» /setwelcome
+» /changewelcome
+» /delsetwelcome
+» /setleft
+» /changeleft
+» /delsetleft
+» /antiwame
+» /antiwame2
+» /antilink
+» /antilink2
+» /open
+» /close
+» /hidetag
+» /add
+» /kick
+» /setppgc
+» /setnamegc
+» /setdesgc
+» /linkgc
+» /resetlinkgc
+» /promote
+» /demote
+» /setbot
+» /updatesetbot
+» /delsetbot
+» /bot
+\`\`\`
+
+📝 *NOTE*: 
+Fitur nya bisa dipakai dengan atau
+tanpa prefix (simbol awalan). Sebagai contoh 
+fitur .owner (prefix)
+dan bisa juga owner (tanpa prefix)
+`
+}
